453e2da01444c26c16dd681a2a1eb746bda0af51
leo <leonardobocci99@hotmail.com>
2024-02-20 17:59:02 +0100

    fix fantasy raw typo

--- dagster_deployment/__init__.py
+++ dagster_deployment/__init__.py
@@ -1,7 +1,7 @@
 from dagster import Definitions, load_assets_from_modules
-from assets import fantasy_raw
+from assets import fantasy_landing
 
-fantasy_assets = load_assets_from_modules([fantasy_raw])
+fantasy_assets = load_assets_from_modules([fantasy_landing])
 
 defs = Definitions(
     assets=[*fantasy_assets],
