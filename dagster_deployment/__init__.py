3a21be5a22d7cdb228d0588a2e47d52b8b325d10
leo <leonardobocci99@hotmail.com>
2024-02-20 17:00:32 +0100

    rm invalid import

--- dagster_deployment/__init__.py
+++ dagster_deployment/__init__.py
@@ -1,6 +1,5 @@
 from dagster import Definitions, load_assets_from_modules
 from assets import fantasy_results
-from resources import fantasy
 
 fantasy_assets = load_assets_from_modules([fantasy_results])
 
