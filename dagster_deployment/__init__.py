ac3ab479e833cedbb3e0454f8c324678932b222e
leo <leonardobocci99@hotmail.com>
2024-02-21 10:41:42 +0100

    auto materialize policy fix

--- dagster_deployment/__init__.py
+++ dagster_deployment/__init__.py
@@ -3,7 +3,7 @@ from assets import fantasy_landing, fantasy_bronze
 
 landing_fantasy_assets = load_assets_from_modules([fantasy_landing])
 #Downstream layers are auto materialized whenever the upstream layer is materialized
-bronze_fantasy_assets = load_assets_from_modules([fantasy_bronze], AutoMaterializePolicy.eager())
+bronze_fantasy_assets = load_assets_from_modules([fantasy_bronze], auto_materialize_policy=AutoMaterializePolicy.eager())
 
 defs = Definitions(
     assets=[*landing_fantasy_assets, *bronze_fantasy_assets],
