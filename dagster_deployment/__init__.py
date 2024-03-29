d8a4f3f340a2e812fd573a2867f25fa4bd3dd6b8
leo <leonardobocci99@hotmail.com>
2024-03-29 15:39:50 +0100

    dagster job definitions

--- dagster_deployment/__init__.py
+++ dagster_deployment/__init__.py
@@ -1,5 +1,6 @@
 from dagster import Definitions, load_assets_from_modules, AutoMaterializePolicy, AutoMaterializeRule
 from assets import fantasy_landing, fantasy_bronze, fastf1_landing, fastf1_bronze
+from jobs import landing_fastf1_full_job, landing_fantasy_full_job
 
 #Downstream layers are auto materialized whenever the upstream layer is materialized
 wait_for_all_parents_policy = AutoMaterializePolicy.eager().with_rules(
@@ -12,6 +13,9 @@ bronze_fantasy_assets = load_assets_from_modules([fantasy_bronze],auto_materiali
 landing_fastf1_assets = load_assets_from_modules([fastf1_landing])
 bronze_fastf1_assets = load_assets_from_modules([fastf1_bronze],auto_materialize_policy=wait_for_all_parents_policy)
 
+all_jobs = [landing_fastf1_full_job, landing_fantasy_full_job]
+
 defs = Definitions(
     assets=[*landing_fantasy_assets, *bronze_fantasy_assets, *landing_fastf1_assets, *bronze_fastf1_assets],
+    jobs=all_jobs
 )
---
+++ dagster_deployment/jobs/__init__.py
@@ -0,0 +1,17 @@
+from dagster import define_asset_job, AssetSelection
+from partitions import fantasy_partitions, fast_f1_season_partitions
+
+fantasy_assets = AssetSelection.groups('raw_fantasy_files')
+fastf1_assets = AssetSelection.groups('raw_fastf1_files')
+
+landing_fastf1_full_job = define_asset_job(
+    name='fastf1_landing_full',
+    partitions_def=fast_f1_season_partitions,
+    selection=fastf1_assets,
+)
+
+landing_fantasy_full_job = define_asset_job(
+    name='fantasy_landing_full',
+    partitions_def=fantasy_partitions,
+    selection=fantasy_assets,
+)
