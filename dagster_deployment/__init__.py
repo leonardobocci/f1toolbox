24424db5aaa49659c4dc59abfac6aaccc8074491
leo <leonardobocci99@hotmail.com>
2024-05-28 14:47:48 +0200

    accept updated schema

--- dagster_deployment/__init__.py
+++ dagster_deployment/__init__.py
@@ -2,15 +2,16 @@ from dagster import Definitions, load_assets_from_modules, AutoMaterializePolicy
 from assets import fantasy_landing, fantasy_bronze, fastf1_landing, fastf1_bronze
 
 #Downstream layers are auto materialized whenever the upstream layer is materialized
-wait_for_all_parents_policy = AutoMaterializePolicy.eager().with_rules(
-    AutoMaterializeRule.skip_on_not_all_parents_updated()
-)
+#materialization_policy = AutoMaterializePolicy.eager().with_rules(
+#    AutoMaterializeRule.skip_on_not_all_parents_updated() #wait for all parents
+#)
+materialization_policy = AutoMaterializePolicy.eager()
 
 landing_fantasy_assets = load_assets_from_modules([fantasy_landing])
-bronze_fantasy_assets = load_assets_from_modules([fantasy_bronze],auto_materialize_policy=wait_for_all_parents_policy)
+bronze_fantasy_assets = load_assets_from_modules([fantasy_bronze],auto_materialize_policy=materialization_policy)
 
 landing_fastf1_assets = load_assets_from_modules([fastf1_landing])
-bronze_fastf1_assets = load_assets_from_modules([fastf1_bronze],auto_materialize_policy=wait_for_all_parents_policy)
+bronze_fastf1_assets = load_assets_from_modules([fastf1_bronze],auto_materialize_policy=materialization_policy)
 
 defs = Definitions(
     assets=[*landing_fantasy_assets, *bronze_fantasy_assets, *landing_fastf1_assets, *bronze_fastf1_assets],
--- dagster_deployment/utils/fastf1_parser.py
+++ dagster_deployment/utils/fastf1_parser.py
@@ -2,6 +2,7 @@ import glob
 import json
 import polars as pl
 import pyarrow.dataset as ds
+from pathlib import Path
 
 def parse_json_signals(context, signal_directory: str) -> pl.DataFrame:
     '''Given an asset subdirectory, load all json files for all years and return a polars dataframe.'''
@@ -11,5 +12,11 @@ def parse_json_signals(context, signal_directory: str) -> pl.DataFrame:
     return df
 
 def parse_parquet_signals(context, signal_directory: str) -> pl.DataFrame:
-    df = pl.scan_parquet(f'data/landing/fastf1/*/{signal_directory}/*.parquet')
+    if signal_directory == 'laps':
+        df = pl.concat([
+            pl.scan_parquet(str(x)).with_columns(pl.col('Deleted').cast(pl.Utf8))
+            for x in Path('data/landing/fastf1').rglob(f'{signal_directory}/*.parquet')
+        ]).collect(streaming=True)
+    else:
+        df = pl.scan_parquet(f'data/landing/fastf1/*/{signal_directory}/*.parquet')
     return df
