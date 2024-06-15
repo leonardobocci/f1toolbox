4c22d21a4046bc2c3f2c161f7136b2ea23cf45bf
leo <leonardobocci99@hotmail.com>
2024-06-15 14:43:15 +0200

    dim assets

--- src/dbt/models/bronze/fantasy/fantasy_constructors.sql
+++ src/dbt/models/bronze/fantasy/fantasy_constructors.sql
@@ -1,2 +1,2 @@
 
-SELECT * FROM file('fantasy/constructors.parquet', 'Parquet')
+SELECT id as asset_id,name as asset_name,active as is_active,color,last_updated FROM file('fantasy/constructors.parquet', 'Parquet')
--- src/dbt/models/bronze/fantasy/fantasy_drivers.sql
+++ src/dbt/models/bronze/fantasy/fantasy_drivers.sql
@@ -1,2 +1,2 @@
 
-SELECT * FROM file('fantasy/drivers.parquet', 'Parquet')
+SELECT id as asset_id,name as asset_name,active as is_active,color,last_updated FROM file('fantasy/drivers.parquet', 'Parquet')
