337dac37aec217aba32ae92203f1ae8ee94a4e3f
leo <leonardobocci99@hotmail.com>
2024-08-31 11:27:25 +0200

    dbt data tests

--- src/dagster/assets/dbt_assets.py
+++ src/dagster/assets/dbt_assets.py
@@ -7,7 +7,9 @@ from src.dagster.dbt_project import dbt_project
 @dbt_assets(manifest=dbt_project.manifest_path)
 def f1toolbox_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from (
-        dbt.cli(["build"], context=context)
+        dbt.cli(
+            ["build"], context=context
+        )  # build creates the dbt model and tests them, run only creates them
         .stream()
         .fetch_column_metadata()
         .fetch_row_counts()
