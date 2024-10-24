8fe543a0608ce721855e425afc4d7ee8bb24ead8
leonardobocci <leonardobocci99@hotmail.com>
2024-10-24 08:56:11 +0200

    vulture dead code cleanup

--- src/dagster/assets/constants.py
+++ src/dagster/assets/constants.py
@@ -1,9 +1,6 @@
 # [Fantasy]
 YEARS = ["2022", "2023", "2024"]
 
-landing_FANTASY_PATH = "data/landing/fantasy"
-BRONZE_FANTASY_PATH = "data/bronze/fantasy"
-
 # [FastF1]
 SEASONS = ["2022", "2023", "2024"]
 SESSIONS = ["1", "2", "3", "4", "5"]
@@ -13,6 +10,3 @@ EVENTS = {
     2023: list(range(1, 23)),  # 22 events
     2024: list(range(1, 25)),  # 24 events
 }
-
-landing_FASTF1_PATH = "data/landing/fastf1"
-BRONZE_FASTF1_PATH = "data/bronze/fastf1"
--- src/dagster/utils/iomanager.py
+++ src/dagster/utils/iomanager.py
@@ -1,13 +1,10 @@
 import json
-import os
-from typing import Union
 
 import gcsfs
 import polars as pl
 from google.cloud import storage
 
 from dagster import InputContext, IOManager, MetadataValue, OutputContext
-from src.dagster.assets import constants
 
 
 def dagster_output_identifier(
@@ -132,66 +129,3 @@ class GCSPolarsParquetIOManager(IOManager):
                     return pl.scan_parquet(f)
             else:
                 raise e
-
-
-def _save_generic_json(data: dict, filename: str, filedir: str) -> None:
-    """Save the raw data extracted from the APIs as json files"""
-    if data:
-        filepath = os.path.join(filedir, f"{filename}.json")
-        os.makedirs(filedir, exist_ok=True)
-        with open(filepath, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-    else:
-        context.log.warning(f"Empty dict received for {filename}")  # noqa: F821
-    return
-
-
-def save_landing_fantasy_json(data: dict, filename: str, year: int = None) -> None:
-    """Save the raw data extracted from the Fantasy APIs as json files"""
-    if year:
-        landing_fantasy_dir = f"{constants.landing_FANTASY_PATH}/{year}"
-    else:
-        landing_fantasy_dir = f"{constants.landing_FANTASY_PATH}/"
-    _save_generic_json(data, filename, landing_fantasy_dir)
-    return
-
-
-def save_landing_fastf1_json(
-    data: dict, filename: str, year: int = None, subdirectory: str = None
-) -> None:
-    """Save the raw data extracted from the FastF1 APIs as json files"""
-    if year:
-        landing_fastf1_dir = f"{constants.landing_FASTF1_PATH}/{year}"
-    else:
-        landing_fastf1_dir = f"{constants.landing_FASTF1_PATH}/"
-    if subdirectory:
-        landing_fastf1_dir = f"{landing_fastf1_dir}/{subdirectory}"
-    _save_generic_json(data, filename, landing_fastf1_dir)
-    return
-
-
-def polars_to_parquet(
-    filedir: str, filename: str, data: Union[pl.DataFrame, pl.LazyFrame], context
-) -> None:
-    """Write a polars frame to parquet file"""
-
-    os.makedirs(filedir, exist_ok=True)
-    if isinstance(data, pl.DataFrame):
-        data.write_parquet(f"{filedir}/{filename}.parquet")
-    elif isinstance(data, pl.LazyFrame):
-        # not supported for all operations so cannot always use
-        try:
-            data.sink_parquet(f"{filedir}/{filename}.parquet")
-        except Exception as e:
-            context.log.debug(
-                f"Could not write parquet file using sink_parquet. Trying to collect and write. {e}"
-            )
-            data.collect(streaming=True).write_parquet(f"{filedir}/{filename}.parquet")
-    else:
-        raise NotImplementedError("Data type not supported")
-    return
-
-
-def parquet_to_polars(filedir: str, context) -> pl.LazyFrame:
-    """Read a parquet file to polars frame"""
-    return pl.scan_parquet(filedir)
