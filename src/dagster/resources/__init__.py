c84d4e92d5d81df0376f775976bd31f52ca5f6c6
leonardobocci <leonardobocci99@hotmail.com>
2024-11-14 09:02:53 +0100

    use envvars connected to k8s secrets

--- src/dagster/resources/__init__.py
+++ src/dagster/resources/__init__.py
@@ -1,15 +1,12 @@
 from dagster_airbyte import AirbyteResource
 
-from src.dagster.resources.retrieve_k8s_secret import retrieve_secret
-
-airbyte_passwords = retrieve_secret("airbyte-auth-secrets", "f1toolbox-core")
-airbyte_username = retrieve_secret("airbyte-username", "f1toolbox-core")
+from dagster import EnvVar
 
 airbyte_instance = AirbyteResource(
     host="airbyte.f1toolbox.com",
     port="",
     # only basic auth is supported as of dagster 1.8.13
-    username=airbyte_username["username"],
-    password=airbyte_passwords["instance-admin-password"],
+    username=EnvVar["username"],
+    password=EnvVar["instance-admin-password"],
     use_https=True,
 )
